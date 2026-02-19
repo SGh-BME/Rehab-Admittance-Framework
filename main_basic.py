1f87bfea3099cd841c97c79471a75eab52705dd0
SGh-BME <156346294+SGh-BME@users.noreply.github.com>
2026-02-19 17:14:13 -0500

    Create main_basic.py

---
+++ main_basic.py
@@ -0,0 +1,15 @@
+# basic admittance 
+
+import pybullet as p
+import pybullet_data
+import time
+import os
+import math
+import threading
+import queue
+from collections import deque
+from enum import Enum, auto
+import tkinter as tk
+from tkinter import ttk
+from typing import Dict, List, Any, Tuple
+import sys
